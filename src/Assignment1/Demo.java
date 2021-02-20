b0c54ac9778530f20dd748137993bb0016df1f7f
Santosh Lal Karna <santosh.karna@dryicesolutions.net>
2021-02-20 09:27:14 +0545

    refactored code

--- src/Assignment1/Demo.java
+++ src/Assignment1/Demo.java
@@ -5,5 +5,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello from Neapl");
         System.out.println("I am Santosh here.");
+        System.out.println("Hello, Iam good.");
     }
 }
